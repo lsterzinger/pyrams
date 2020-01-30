ce7acd75d420e8808859a64f3aa50d7401059ac6
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2020-01-29 16:00:22 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ramslibs',
-      version='0.5',
+with open('README.md', 'r') as fh:
+    long_description=fh.read()
+
+setuptools.setup(name='ramslibs',
+      version='0.5.1',
       description='Set of tools for working with RAMS data',
+      long_description=long_description,
+      long_description_content='text/markdown',
+      url='https://github.com/lsterzinger/ramslibs',
       author='Lucas Sterzinger',
       author_email='lsterzinger@ucdavis.edu',
-      packages=['ramslibs'],
-      install_requires=[
-          'netCDF4',
-          'numpy',
-          'matplotlib',
-          'metpy',
-          'glob2',
-          'tqdm'
+      packages=setuptools.find_packages(),
+      classifiers= [
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+          'Development Status :: 4 - Beta'
       ]
       )
