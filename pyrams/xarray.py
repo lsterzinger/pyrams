24a8d349e487a964423035bfe5b88cb105458b95
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2022-05-13 12:29:55 -0700

    Started work on variable metadata

--- pyrams/xarray.py
+++ pyrams/xarray.py
@@ -1,3 +1,4 @@
+from logging import warning
 import xarray as xr
 @xr.register_dataset_accessor("rams")
 class RAMSAccessor:
@@ -21,3 +22,57 @@ class RAMSAccessor:
     @property
     def iwp(self):
         return self.iwc.integrate('z')
+
+    def apply_metadata(self):
+        import json
+
+        ds = self._obj
+
+        with open('./rams-vars.json', 'r') as inf:
+            ramsvars = json.loads(inf.read())
+        
+        nokey = []
+        for v in ds.variables:
+            if v in ramsvars.keys():
+                ds[v].attrs['unit'] = ramsvars[v]['unit']
+                ds[v].attrs['long_name'] = ramsvars[v]['long_name']
+            else: 
+                nokey.append(v)
+
+        if len(nokey) > 0:
+            raise Warning(f"No metadata found for variables {nokey}")
+
+
+    def fix_dims(
+        self,
+        flist=None, 
+        dx=None,
+        dims = {
+            'phony_dim_0' : 'x',
+            'phony_dim_1' : 'y',
+            'phony_dim_2' : 'z'
+        },
+        dz=None,
+        z=None):
+
+        from pyrams.data_tools import flist_to_times
+        import numpy as np
+
+        ds = self._obj.copy()
+        
+        if flist and ds['time']:
+            ds['time'] = flist_to_times(flist)
+        
+        if dx:
+            Warning('dx')
+            ds['x'] = np.arange(0, len(ds.x)) * dx
+            ds['y'] = np.arange(0, len(ds.y)) * dx
+
+        if dz:
+            Warning('dz')
+            ds['z'] = np.arange(0, len(ds.z))*dz 
+        elif z:
+            Warning('z')
+            ds['z'] = z 
+
+        return ds
